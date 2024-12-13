6e37b370b6d269f87b18a206c6417ff53fc8c515
Scott McCoy <scotthmccoy@gmail.com>
2024-12-13 14:17:14 -0800

    feat: Added XCTAssertEqualEncodableOption
    Defaults to assumeExpectedIsCorrect, which guides the user to make changes to actual.

--- Code/XCTest/XCTAsserts.swift
+++ Code/XCTest/XCTAsserts.swift
@@ -38,9 +38,23 @@ public func XCTAssertStartsWith(
 /**
 Improvement over XCTAssertEqual for use with Encodable arguments (see SwiftInitEncoder)
  */
+public enum XCTAssertEqualEncodableOption {
+    case assumeExpectedIsCorrect
+    case assumeActualIsCorrect
+    
+    var makeChangesTo: String {
+        switch self {
+            case .assumeActualIsCorrect: return "actual"
+            case .assumeExpectedIsCorrect: return "expected"
+        }
+    }
+}
+
+
 public func XCTAssertEqualEncodable<T: Encodable>(
     expected: T,
     actual: T,
+    option: XCTAssertEqualEncodableOption = .assumeActualIsCorrect,
     file: StaticString = #filePath,
     line: UInt = #line
 ) where T: Equatable {
@@ -65,9 +79,20 @@ public func XCTAssertEqualEncodable<T: Encodable>(
     // and convert to a git diff style format
     let expectedInitStatementLines = expectedInitStatement.split(separator: "\n")
     let actualInitStatementLines = actualInitStatement.split(separator: "\n")
-    let strDiff = expectedInitStatementLines.difference(
-        from: actualInitStatementLines
-    ).map {
+    
+    var collectionDifference: CollectionDifference<Substring>
+    switch option {
+        case .assumeActualIsCorrect:
+            collectionDifference = expectedInitStatementLines.difference(
+                from: actualInitStatementLines
+            )
+        case .assumeExpectedIsCorrect:
+            collectionDifference = expectedInitStatementLines.difference(
+                from: actualInitStatementLines
+            )
+    }
+    
+    let strDiff = collectionDifference.map {
         switch $0 {
         case .insert(let offset, let element, associatedWith: _):
             return "\(offset): + \(element)"
@@ -79,7 +104,7 @@ public func XCTAssertEqualEncodable<T: Encodable>(
     
     XCTFail(
         // swiftlint:disable:next line_length
-        "\nactual:\n\(actualInitStatement)\n\nexpected:\n\(expectedInitStatement)\n\nMake the following changes:\n\(strDiff)",
+        "\nactual:\n\(actualInitStatement)\n\nexpected:\n\(expectedInitStatement)\n\nMake the following changes to \(option.makeChangesTo):\n\(strDiff)",
         file: file,
         line: line
     )
